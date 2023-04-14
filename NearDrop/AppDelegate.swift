9216f4e718ffcedc1178a8f2d506c7d24e0ca435
Grishka <git@grishka.me>
2023-04-14 05:46:34 +0300

    Show an alert when notifications permission is denied

--- NearDrop/AppDelegate.swift
+++ NearDrop/AppDelegate.swift
@@ -26,6 +26,9 @@ class AppDelegate: NSObject, NSApplicationDelegate{
 		
 		let nc=UNUserNotificationCenter.current()
 		nc.requestAuthorization(options: [.alert, .sound]) { granted, err in
+			if !granted{
+				self.showNotificationsDeniedAlert()
+			}
 		}
 		let incomingTransfersCategory=NDNotificationCenterHackery.hackedNotificationCategory()
 		let errorsCategory=UNNotificationCategory(identifier: "ERRORS", actions: [], intentIdentifiers: [])
@@ -40,5 +43,20 @@ class AppDelegate: NSObject, NSApplicationDelegate{
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
+	
+	func showNotificationsDeniedAlert(){
+		let alert=NSAlert()
+		alert.alertStyle = .critical
+		alert.messageText=NSLocalizedString("NotificationsDenied.Title", value: "Notification Permission Required", comment: "")
+		alert.informativeText=NSLocalizedString("NotificationsDenied.Message", value: "NearDrop needs to be able to display notifications for incoming file transfers. Please allow notifications in System Settings.", comment: "")
+		alert.addButton(withTitle: NSLocalizedString("NotificationsDenied.OpenSettings", value: "Open settings", comment: ""))
+		alert.addButton(withTitle: NSLocalizedString("Quit", value: "Quit NearDrop", comment: ""))
+		let result=alert.runModal()
+		if result==NSApplication.ModalResponse.alertFirstButtonReturn{
+			NSWorkspace.shared.open(URL(string: "x-apple.systempreferences:com.apple.preference.notifications")!)
+		}else if result==NSApplication.ModalResponse.alertSecondButtonReturn{
+			NSApplication.shared.terminate(nil)
+		}
+	}
 }
 
